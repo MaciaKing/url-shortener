1273168826d3b2f31267c4e5a455f94d21c2b294
MaciaKing <maciasalvasalva@gmail.com>
2024-07-06 20:52:36 +0200

    [feat]: datatables is loading last data

--- static/js/index.js
+++ static/js/index.js
@@ -18,37 +18,36 @@ function addOrHiddeContent(button0Cheked){
     }
 }
 
+function printTable() {
+    $.ajax({
+        url: 'http://localhost:8000/url/custom_urls',
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            createTable(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('There has been a problem with your fetch operation:', textStatus, errorThrown);
+        }
+    });
+}
+
+function createTable(data) {
+    $('#table').DataTable({
+        data: data,
+        columns: [
+            { data: "custom_url" },
+            { data: "url" }
+        ]
+    });
+}
+
 window.onload = function() {
     var id_shortcut_option_0_button = document.getElementById("id_shortcut_option_0");
     var id_shortcut_option_1_button = document.getElementById("id_shortcut_option_1");
 
     id_shortcut_option_0_button.addEventListener("change", detectRadioButtonSelected);
     id_shortcut_option_1_button.addEventListener("change", detectRadioButtonSelected);
-    createDataTable();
-}
-
-
-function createDataTable(){
-    const data = [
-        {
-            'url': 'https://chatgpt.com/c/60893c8e-53c6-40c5-94cd-76bbc2f0d2cc',
-            'custom_url': 'pyrz'
-        },
-        {
-            'url': 'https://chatgpt.com/c/60893c8e-53c6-40c5-94cd-76bbc2f0d2cc',
-            'custom_url': 'uoud'
-        },
-        {
-            'url': 'https://chatgpt.com/c/60893c8e-53c6-40c5-94cd-76bbc2f0d2cc',
-            'custom_url': 'bhbr'
-        }
-    ];
-    // Initialize DataTable
-    $('#example').DataTable({
-        data: data,
-        columns: [
-            { data: 'url' },
-            { data: 'custom_url' }
-        ]
-    });
+    
+    printTable();
 }
