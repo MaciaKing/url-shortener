da8ba7187e992492d16e14e784b483b2ab9384ce
MaciaKing <maciasalvasalva@gmail.com>
2024-07-06 21:49:48 +0200

    [feat]: add bootsrap components

--- url_logic/forms.py
+++ url_logic/forms.py
@@ -7,6 +7,6 @@ class UrlForm(forms.Form):
         ('manual', 'Enter manually'),
     ]
     
-    url = forms.URLField(label='Enter URL', max_length=200)
-    shortcut_option = forms.ChoiceField( choices=CHOICES, widget=forms.RadioSelect)
-    shortcut_url = forms.CharField(label='Custom Shortcut URL', max_length=100, required=False)
+    url = forms.URLField(label='Enter URL', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Default URL'}))
+    shortcut_option = forms.ChoiceField( choices=CHOICES, widget=forms.RadioSelect(attrs={ 'class':'form-check-input', 'type':'radio'}))
+    shortcut_url = forms.CharField(label='Enter URL', max_length=200, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Custom shortener'}))
