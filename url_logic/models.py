17b5a66452c8e272d25952b0f4b69804ceeb2064
MaciaKing <maciasalvasalva@gmail.com>
2024-07-07 19:26:16 +0200

    [feat]: add save_url staticmethod

--- url_logic/models.py
+++ url_logic/models.py
@@ -11,6 +11,17 @@ class CustomUrl(models.Model):
         return self.url
     
     @staticmethod
+    def save_url(custom_url, url):
+        if CustomUrl.objects.filter(custom_url=custom_url).exists():
+            raise Exception("Custom url already exist")
+        else:
+            cm = CustomUrl(
+                custom_url = custom_url,
+                url = url
+            )
+            cm.save()
+
+    @staticmethod
     def generate_random_custom_url():
         letters = string.ascii_lowercase  # 'abcdefghijklmnopqrstuvwxyz'
         return''.join(random.choice(letters) for _ in range(4))  # Genera una cadena aleatoria de 4 letras minúsculas
