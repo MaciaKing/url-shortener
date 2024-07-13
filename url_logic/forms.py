e4c1b2d05ac5cae45fe7da49616fa08606b79302
MaciaKing <maciasalvasalva@gmail.com>
2024-07-13 17:30:00 +0200

    [feat]: refactor like flake8 says

--- url_logic/forms.py
+++ url_logic/forms.py
@@ -1,12 +1,33 @@
 from django import forms
 
+
 class UrlForm(forms.Form):
-    #url = forms.URLField(label='Enter URL', max_length=200)
     CHOICES = [
         ('auto', 'Generate automatically'),
         ('manual', 'Enter manually'),
     ]
-    
-    url = forms.URLField(label='Enter URL', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Default URL'}))
-    shortcut_option = forms.ChoiceField( choices=CHOICES, widget=forms.RadioSelect(attrs={ 'class':'form-check-input', 'type':'radio'}))
-    shortcut_url = forms.CharField(label='Enter URL', max_length=200, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Custom shortener'}))
+
+    url = forms.URLField(
+        label='Enter URL',
+        max_length=200,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Default URL'}))
+
+    shortcut_option = forms.ChoiceField(
+        choices=CHOICES,
+        widget=forms.RadioSelect(
+            attrs={
+                'class': 'form-check-input',
+                'type': 'radio'
+            }
+        )
+    )
+
+    shortcut_url = forms.CharField(
+        label='Enter URL',
+        max_length=200,
+        required=False,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Custom shortener'}
+            )
+    )
