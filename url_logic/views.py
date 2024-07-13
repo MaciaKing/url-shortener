09b698ccf1a74e5df336dbe05e32e9bc365614b8
MaciaKing <maciasalvasalva@gmail.com>
2024-07-13 17:53:41 +0200

    [refactor]: add flask8 format to views file

--- url_logic/views.py
+++ url_logic/views.py
@@ -3,10 +3,7 @@ from .forms import UrlForm
 from .models import CustomUrl
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-# from django.shortcuts import redirect
-from django.shortcuts import redirect as django_redirect
 
-import ipdb
 
 # Create your views here.
 def index(request):
@@ -18,26 +15,47 @@ def index(request):
             # get shortcut url
             custom_url = request.POST['shortcut_url']
         try:
-            cu = CustomUrl(custom_url = custom_url, url = request.POST['url'])
+            cu = CustomUrl(custom_url=custom_url, url=request.POST['url'])
             cu.save()
             custom_urls = CustomUrl.objects.all()
             form = UrlForm()
-        except:
-            return render(request, 'index.html', {'form': form, 'custom_urls': custom_urls, 'exception': True})
-        
+        except Exception as e:
+            return render(
+                request,
+                'index.html',
+                {
+                    'form': form,
+                    'custom_urls': custom_urls,
+                    'exception': True,
+                    'error': str(e)
+                }
+            )
+
     custom_urls = CustomUrl.objects.all()
     form = UrlForm()
-    return render(request, 'index.html', {'form': form, 'custom_urls': custom_urls, 'exception': False})
+    return render(
+        request,
+        'index.html',
+        {
+            'form': form,
+            'custom_urls': custom_urls,
+            'exception': False
+        }
+    )
+
 
 def get_all_custom_urls(request):
     custom_urls = list(CustomUrl.objects.values())
     return JsonResponse(custom_urls, safe=False)
 
+
 @csrf_exempt
 def drop_custom_url(request):
     if request.method == 'POST':
         try:
-            CustomUrl.objects.get(custom_url=request.POST['custom_url']).delete()
+            CustomUrl.objects.get(
+                custom_url=request.POST['custom_url']
+            ).delete()
             response = {'status': 'success'}
         except CustomUrl.DoesNotExist:
             response = {'status': 'error', 'message': 'Custom URL not found'}
