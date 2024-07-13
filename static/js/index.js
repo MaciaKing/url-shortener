ee31e9b53ba5616f5b52ee14ade882631fc70f9c
MaciaKing <maciasalvasalva@gmail.com>
2024-07-13 12:02:43 +0200

    [feat]: redirection function is working

--- static/js/index.js
+++ static/js/index.js
@@ -39,20 +39,30 @@ function createTable(data) {
             { data: "custom_url" },
             { data: "url" },
             {
-                // Define new column for buttons
+                // Delete button column
                 data: null,
-                className: 'text-center', // Center content button
+                className: 'text-center',
                 render: function (data, type, row) {
                     return `<button type="button" class="btn btn-danger delete-button" data-url="${row.custom_url}">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                                 </svg>
+                            </button>
+                            <button type="button" class="btn btn-primary arrow-button" data-url="${row.url}" onclick="arrowButtonClicked('${row.url}')">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+                                </svg>
                             </button>`;
                 }
             }
         ]
     });
+}
+
+function arrowButtonClicked(url){
+    console.log(url)
+    window.open(url, '_blank');
 }
 
 function viewException(exeption){
